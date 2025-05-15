65ac37eb143269827091a1f01c4ac55cb99ecd1f
infrunnami <correoepicgames9@gmail.com>
2025-05-14 22:36:25 -0400

    integracion todos los simuladores

--- src/controllers/simulador.controller.js
+++ src/controllers/simulador.controller.js
@@ -4,6 +4,64 @@ import { driverInfo } from "../services/driver.service.js";
 import { getGiroyOdometro } from "../services/giroscopio.service.js";
 
 class SimuladorController {
+    static async previewSimulador85(req, res) {
+        try{
+            //obtener hash
+            const hash = await getHashDB();
+            if (!hash) throw new Error("No se pudo obtrener el hash");
+
+
+            //obtener dispositivos
+            const devices = await TrackermasgpsService.getTrackerList(hash);
+            const posiciones = [];
+
+            for (const device of devices) {
+                const trackerId = device.id;
+
+                const [estado, conductor, sensores] = await Promise.all([
+                    TrackermasgpsService.getTrackerState(trackerId, hash),
+                    driverInfo(trackerId, hash),
+                    getGiroyOdometro(trackerId, hash),
+                ]);
+                
+                if (!estado?.gps?.location) continue;
+
+                const now = new Date();
+                const last = new Date(estado.last_update);
+                const edadDato = Math.floor((now - last) / 1000);
+
+                const rut = conductor.rut_conductor?.replace("-", "") ?? "00000000";
+                const key = conductor.hardware_key ?? "0000000000000000";
+                const key_button8 = key.slice(-8);
+
+                posiciones.push({
+                    patente: (device.label ?? "").slice(0, 7).replace("-", ""),
+                    fecha_hora: new Date().toISOString().slice(0, 19).replace("T", " "),
+                    latitud: `${estado.gps.location.lat}`.replace(".", ","),
+                    longitud: `${estado.gps.location.lng}`.replace(".", ","),
+                    direccion: `${estado.gps.heading ?? 0}`,
+                    velocidad: `${estado.gps.speed ?? 0}`.replace(".", ","),
+                    estado_registro: "1",
+                    estado_ignicion: "1", // forzado
+                    numero_evento: "45",  // forzado
+                    odometro: `${sensores.odometro}`.replace(".", ","),
+                    numero_satelites: `${Math.floor(Math.random() * 6 + 10)}`,
+                    hdop: "1",
+                    edad_dato: `${edadDato}`,
+                    rut_conductor: key_button8,
+                    nombre_conductor: rut,
+                })
+            }
+            return res.json({ posicion: posiciones });
+
+        } catch (error) {
+            console.error("error en previewSimulador85: ", error.message);
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+
+
     static async previewSimuladorFrenado(req, res) {
         try {
             //obtener hash
@@ -58,6 +116,65 @@ class SimuladorController {
             res.status(500).json({ error: error.message });
         }
     } 
+
+    static async previewSimuladorFrenado2(req, res) {
+        try {
+            //obtener hash
+            const hash = await getHashDB();
+            if (!hash) throw new Error("No se pudo obtrener el hash");
+
+            //obtener dispositivos
+            const devices = await TrackermasgpsService.getTrackerList(hash);
+            const posiciones = [];
+
+            for (const device of devices) {
+                const trackerId = device.id;
+
+                const [estado, conductor, sensores] = await Promise.all([
+                    TrackermasgpsService.getTrackerState(trackerId, hash),
+                    driverInfo(trackerId, hash),
+                    getGiroyOdometro(trackerId, hash),
+                ]);
+
+                if (!estado?.gps?.location) continue;
+
+                const now = new Date();
+                const last = new Date(estado.last_update);
+                const edadDato = Math.floor((now - last) / 1000);
+
+                const rut = conductor.rut_conductor?.replace("-", "") ?? "00000000";
+                const key = conductor.hardware_key ?? "0000000000000000";
+                const key_button8 = key.slice(-8);
+                const opcionalNegativo = (Math.floor(Math.random() * (740 - 700 + 1)) + 700) / -100;
+
+                posiciones.push({
+                    patente: (device.label ?? "").slice(0, 7).replace("-", ""),
+                    fecha_hora: new Date().toISOString().slice(0, 19).replace("T", " "),
+                    latitud: `${estado.gps.location.lat}`.replace(".", ","),
+                    longitud: `${estado.gps.location.lng}`.replace(".", ","),
+                    direccion: `${estado.gps.heading ?? 0}`,
+                    velocidad: "11",
+                    estado_registro: "1",
+                    estado_ignicion: "1",
+                    numero_evento: "51",
+                    odometro: `${sensores.odometro}`.replace(".", ","),
+                    numero_satelites: `${Math.floor(Math.random() * 6 + 10)}`,
+                    hdop: "1",
+                    edad_dato: `${edadDato}`,
+                    rut_conductor: key_button8,
+                    nombre_conductor: rut,
+                    opcional_1: `${opcionalNegativo}`.replace(".", ","),
+                });
+            }
+
+            return res.json({ posicion: posiciones });
+        } catch (error) {
+            console.error("error en previewSimuladorFrenado2: ", error.message);
+            res.status(500).json({ error: error.message });
+        }
+    }
 }
+
+
 
 export default SimuladorController;
--- src/routes/simulador.routes.js
+++ src/routes/simulador.routes.js
@@ -3,6 +3,10 @@ import SimuladorController from "../controllers/simulador.controller.js";
 
 const router = Router();
 
-router.get("/frenado", SimuladorController.previewSimuladorFrenado);
+router.get("/simulador85", SimuladorController.previewSimulador85);
+
+router.get("/frenado1", SimuladorController.previewSimuladorFrenado);
+
+router.get("/frenado2", SimuladorController.previewSimuladorFrenado2);
 
 export default router;
